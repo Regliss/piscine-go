393676ef17e6114c1734605ef72226b25c748476
ars192 <amanzholov.arsytan@gmail.com>
2020-02-19 17:59:08 +0600

    hehe boy5

--- printcomb.go
+++ printcomb.go
@@ -6,8 +6,13 @@ func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				if i < j {
-					if j < k {
+				if ( i < j && j < k ) {
+					if ( i == '7' && j == '8' && k == '9') {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(k)
+						z01.PrintRune('\n')
+					} else {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(k)
