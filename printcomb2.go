0f614f3b13aa9d018a8e732142a8349e150a499b
ars192 <amanzholov.arsytan@gmail.com>
2020-02-19 19:24:49 +0600

    comb2last

--- printcomb2.go
+++ printcomb2.go
@@ -7,24 +7,24 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
-					if i != k || j != l {
-						if i == '9' && j == '8' && k == '9' && l == '9' {
-							z01.PrintRune(i)
-							z01.PrintRune(j)
-							z01.PrintRune(32)
-							z01.PrintRune(k)
-							z01.PrintRune(l)
-							z01.PrintRune('\n')
-							return
-						} else {
-							z01.PrintRune(i)
-							z01.PrintRune(j)
-							z01.PrintRune(32)
-							z01.PrintRune(k)
-							z01.PrintRune(l)
-							z01.PrintRune(',')
-							z01.PrintRune(32)
-						}
+					if i == '9' && j == '8' && k == '9' && l == '9' {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(32)
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						z01.PrintRune('\n')
+						z01.PrintRune('1'+'2')
+						return
+					}
+					if int(i) * 10 + int(j) < int(k) * 10 + int(l) {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(32)
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						z01.PrintRune(',')
+						z01.PrintRune(32)
 					}
 				}
 			}
