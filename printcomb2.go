894c8853a5f5ca8dce884dd2a67c0fd4df9a3152
ars192 <amanzholov.arsytan@gmail.com>
2020-02-19 18:31:31 +0600

    pc2

---
+++ printcomb2.go
@@ -0,0 +1,33 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
+					if (i != k || j != l) {
+						if i == '9' && j == '8' && k == '9' && l == '9' {
+							z01.PrintRune(i)
+							z01.PrintRune(j)
+							z01.PrintRune(32)
+							z01.PrintRune(k)
+							z01.PrintRune(l)
+							z01.PrintRune('\n')
+							return
+						} else {
+							z01.PrintRune(i)
+							z01.PrintRune(j)
+							z01.PrintRune(32)
+							z01.PrintRune(k)
+							z01.PrintRune(l)
+							z01.PrintRune(',')
+							z01.PrintRune(32)
+						}
+					}
+				}
+			}
+		}
+	}
+}
