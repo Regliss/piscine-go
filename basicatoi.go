1c4a54069844f604f6cebd82833ddf0f08af6954
ars192 <amanzholov.arsytan@gmail.com>
2020-02-20 19:26:48 +0600

    quest03.09

--- basicatoi.go
+++ basicatoi.go
@@ -4,7 +4,7 @@ func BasicAtoi(s string) int {
 	var result int = 0
 	Array := []rune(s)
 	for i := range Array {
-		result = result * 10 + int(Array[i]) - '0'
+		result = result*10 + int(Array[i]) - '0'
 	}
 	return result
 }
