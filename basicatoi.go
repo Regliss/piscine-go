0316f46f0387cfd8d275ea380497ad21e1206da9
ars192 <amanzholov.arsytan@gmail.com>
2020-02-20 19:20:06 +0600

    quest03.9

---
+++ basicatoi.go
@@ -0,0 +1,10 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	var result int = 0
+	Array := []rune(s)
+	for i := range Array {
+		result = result*10 + int(Array[i]) - '0'
+	}
+	return result
+}
