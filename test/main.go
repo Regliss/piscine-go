20fb2ba930ce8a00e0127da68fa96581df7c8ebf
ars192 <amanzholov.arsytan@gmail.com>
2020-02-20 15:34:30 +0600

    quest03.03

--- test/main.go
+++ test/main.go
@@ -7,7 +7,17 @@ import (
 )
 
 func main() {
-	n := 0
-	piscine.PointOne(&n)
-	fmt.Println(n)
+	// a := 0
+	// b := &a
+	// n := &b
+	// piscine.UltimatePointOne(&n)
+	// fmt.Println(a)
+
+	a := 13
+	b := 2
+	var div int
+	var mod int
+	piscine.DivMod(a, b, &div, &mod)
+	fmt.Println(div)
+	fmt.Println(mod)
 }
